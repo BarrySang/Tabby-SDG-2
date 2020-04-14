84c43f4ad87b934cd161863a2e4ce3a44367805a
BarrySang <barrysang8@gmail.com>
2020-04-14 13:46:17 +0300

    commit

--- src/output-variables.js
+++ src/output-variables.js
@@ -3,7 +3,8 @@ import data from './data';
 const output = {
   data,
   impact: {
-    currentlyInfected: null, infectionsByRequestedTime: null, severeCasesByRequestedTime: null, hospitalBedsByRequestedTime: null, casesForICUByRequestedTime: null, casesForVentilatorsByRequestedTime: null, dollarsInFlight: null
+    currentlyInfected: null,
+    infectionsByRequestedTime: null, severeCasesByRequestedTime: null, hospitalBedsByRequestedTime: null, casesForICUByRequestedTime: null, casesForVentilatorsByRequestedTime: null, dollarsInFlight: null
   },
   severeImpact: {
     currentlyInfected: null, infectionsByRequestedTime: null, severeCasesByRequestedTime: null, hospitalBedsByRequestedTime: null, casesForICUByRequestedTime: null, casesForVentilatorsByRequestedTime: null, dollarsInFlight: null
